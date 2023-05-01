38cd187909a76baf9b2745feaa10e009cac4245b
Nriver95 <123195590+Nriver95@users.noreply.github.com>
2023-05-01 15:23:04 -0400

    fixed links in index.html
    Fixed the links in index.html

---
+++ BasicJavaScriptProjects/Project1_expressions_alert/js/main.js
@@ -0,0 +1,7 @@
+var A = '\'This is a string.\' '
++'There are many like it but this one is mine. '
++'This string is my best friend. '
++'I must master it as I must master my life.'
+window.alert(A);
+
+document.write(2+2)
