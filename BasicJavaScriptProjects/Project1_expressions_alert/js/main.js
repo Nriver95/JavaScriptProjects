6e7f8a67296ac2c2e8caae11fe4e007726114a4a
Nriver95 <123195590+Nriver95@users.noreply.github.com>
2023-05-02 16:34:54 -0400

    created index and js file
    created the index.html file and main.js file for the Project1 expressions alert project.

--- BasicJavaScriptProjects/Project1_expressions_alert/js/main.js
+++ BasicJavaScriptProjects/Project1_expressions_alert/js/main.js
@@ -1,7 +1,15 @@
-var A = '\'This is a string.\' '
-+'There are many like it but this one is mine. '
+var A = '\'This is a string.\' '                //String assigned to an element
++'There are many like it but this one is mine. '//String concatenated with a plus sign
 +'This string is my best friend. '
 +'I must master it as I must master my life.'
-window.alert(A);
+window.alert(A);                                //button using the variable A
 
-document.write(2+2)
+document.write(2+2)                             //Expression adding 2 and 2 together to make 4
+
+function myFirstFunction() {                //Defining a function and naming it
+    var str = 'This is the button text!';   //Defining a variable and giving it
+                                            //a string value
+    document.getElementById('Button_Text').innerHTML = str; //Putting the value
+                                            //of the variable into the HTML elementFromPoint
+                                            //with the "Button_Text" id
+}
