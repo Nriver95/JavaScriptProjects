18333b262a9d7a7609c74b636a4c4421922001cc
Nriver95 <123195590+Nriver95@users.noreply.github.com>
2023-05-06 18:36:37 -0400

    updated main.js
    Updated main.js with comments explaining the code.

--- BasicJavaScriptProjects/Project4_dictionaries/JS/main.js
+++ BasicJavaScriptProjects/Project4_dictionaries/JS/main.js
@@ -1,11 +1,11 @@
-function monsterList(){
-    var monster={
-        family:'bird wyvern',
+function monsterList(){         //creates function called monsterList
+    var monster={               //creates our dictionary variable called monster
+        family:'bird wyvern',   //addes key value pairs
         species:'Yian Kut-ku',
         size:'small',
         element:'fire',
         weakness:'water',
     };
-    delete monster.species;
-    document.getElementById("dictionary").innerHTML = monster.species;
+    delete monster.species;     //deletes the kvp defined by 'species'
+    document.getElementById("dictionary").innerHTML = monster.species;  //allows the html doc to get the element 'species' found within the variable 'monster' and writes it in the place using the id 'dictionary'
 }
