0a1f166288aca591eba7dcbf0563e235370b086d
Nriver95 <123195590+Nriver95@users.noreply.github.com>
2023-05-06 20:31:47 -0400

    added project5 folder
    added the Project5 folder with the index.html and JS folder inside.  The JS folder contains main.js.  Demonstrated use of comparison type coercion.

---
+++ BasicJavaScriptProjects/Project5_comparisons_types_coercion/JS/main.js
@@ -0,0 +1,55 @@
+document.write(typeof "10");
+
+document.write('number'+7);
+
+document.write(2E310);
+
+document.write(-2E310);
+
+document.write(10>2);
+
+document.write(2>10);
+
+console.log(2+2);
+
+console.log(365<1);
+
+console.log(365==1);
+
+console.log(365==365);
+
+x=10;
+y=10;
+document.write(x===y);
+
+a=10;
+b='10';
+document.write(a===b);
+
+c='Rivera';
+d='Rivera';
+document.write(c===d);
+
+document.write(5==5 && 10>4);
+
+document.write(5==4 && 10>4);
+
+document.write(5==4 || 10>4);
+
+document.write(5==4 || 10<4);
+function myFunction() {
+    document.getElementById('test').innerHTML =0/0;
+}
+
+function notFunction() {
+    document.getElementById('not').innerHTML = !(10>1);
+}
+
+function notNotFunction() {
+    document.getElementById('notNot').innerHTML = !(5>10);
+}
+
+//document.getElementById('test').innerHTML=isNaN('this is a string');
+
+//document.getElementById('test').innerHTML=isNaN('007');
+
