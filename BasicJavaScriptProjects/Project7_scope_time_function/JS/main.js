25943f83a78da4c19fd44d274cc81480f936e236
Nriver95 <123195590+Nriver95@users.noreply.github.com>
2023-05-13 20:24:24 -0400

    added project 7 folder and files
    added project 7 folder with index.html file and JS folder which contains the main.js file

---
+++ BasicJavaScriptProjects/Project7_scope_time_function/JS/main.js
@@ -0,0 +1,20 @@
+var x=10;
+function addNumbers1(){
+    document.write(20+x+"<br>");
+}
+function addNumbers2(){
+    document.write(x+100);
+}
+addNumbers1();
+addNumbers2();
+
+
+function addNumbers5(){
+    var a=10
+    console.log(20+a+"<br>");
+}
+function addNumbers6(){
+    console.log(a+100);
+}
+addNumbers5();
+addNumbers6();
