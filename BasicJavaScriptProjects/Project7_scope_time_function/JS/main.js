62ca7669aace3f1e05bb0c33c6ed156b3dcfc287
Nriver95 <123195590+Nriver95@users.noreply.github.com>
2023-05-13 21:58:12 -0400

    added code to project7 files
    added code to project7 html and js files.

--- BasicJavaScriptProjects/Project7_scope_time_function/JS/main.js
+++ BasicJavaScriptProjects/Project7_scope_time_function/JS/main.js
@@ -3,18 +3,17 @@ function addNumbers1(){
     document.write(20+x+"<br>");
 }
 function addNumbers2(){
-    document.write(x+100);
+    document.write(x+100+"<br>");
 }
 addNumbers1();
 addNumbers2();
 
+if (1<2){
+    document.write("The left number is smaller than the number on the right.")
+}
 
-function addNumbers5(){
-    var a=10
-    console.log(20+a+"<br>");
+function getDate(){
+    if (new Date().getHours()>18){
+        document.getElementById("Greeting").innerHTML="How are you today?";
+    }
 }
-function addNumbers6(){
-    console.log(a+100);
-}
-addNumbers5();
-addNumbers6();
