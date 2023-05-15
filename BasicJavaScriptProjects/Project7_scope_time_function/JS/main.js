11a3c6c9de56fed25c8ca50675800ecfa0cfb26b
Nriver95 <123195590+Nriver95@users.noreply.github.com>
2023-05-15 16:31:28 -0400

    fixed project 7 code
    adjusted placement of script element on html doc and also added comments as well as the function error with comments as to how I would fix it.

--- BasicJavaScriptProjects/Project7_scope_time_function/JS/main.js
+++ BasicJavaScriptProjects/Project7_scope_time_function/JS/main.js
@@ -1,19 +1,30 @@
-var x=10;
-function addNumbers1(){
-    document.write(20+x+"<br>");
+var x=10;           //creates variable x and sets it to the value 10
+function addNumbers1(){ //creates a function addNumbers1 
+    document.write(20+x+"<br>");    //the function will write the results of the math being done onto the index.html
 }
-function addNumbers2(){
+function addNumbers2(){ //another function
     document.write(x+100+"<br>");
 }
-addNumbers1();
+addNumbers1();  //executes the function
 addNumbers2();
 
-if (1<2){
-    document.write("The left number is smaller than the number on the right.")
+if (1<2){       //creates a conditional statement with the argument that the value 1 is less than the value 2.
+    document.write("The left number is smaller than the number on the right." + "<br>")  //the if statement will perform the function defined here if it is true.
 }
 
 function getDate(){
-    if (new Date().getHours()>18){
-        document.getElementById("Greeting").innerHTML="How are you today?";
+    if (new Date().getHours()>18){      //creates a conditional statement to see if the time currently is greater than 18 or 1800 hours
+        document.getElementById("Greeting").innerHTML="How are you today?";     //if the statement is true it will perform the function as called by the onclick attribute.
     }
 }
+
+function addNumbers3(){ //creates function
+    var y=10;   //defines variable y
+    document.write(20 + y + "<br>");    //defines a math problem to be solved and written onto the html doc
+}
+function addNumbers4(){
+    document.write(y + 100);    //defines a math problem to be solved and the results written onto the html doc but the variable is not global and the function will not work.
+}
+addNumbers3();
+addNumbers4();
+//To solve the function I would move the variable defined within the function addNumbers3 and move it outside of any function so it will function globally.
